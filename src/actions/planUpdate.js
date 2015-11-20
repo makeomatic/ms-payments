58e3a51757e7f7dc6b61a5fa3387b11ae8e4313a
Dmitry Usa <atskiisotona@gmail.com>
2015-11-20 23:01:10 +0800

    Added planUpdate

--- src/actions/planUpdate.js
+++ src/actions/planUpdate.js
@@ -3,15 +3,34 @@ const Errors = require('common-errors')
 
 const paypal = require('paypal-rest-sdk')
 
-function planUpdate(planId) {
+function planUpdate(planId, query) {
 	const {
 		_redis: redis,
-		_config: config
+		_config: config,
+		_ajv: ajv
 	} = this
 
 	let promise = Promise.bind(this)
 
-	return promise
+	function sendRequest() {
+		return Promise.create((resolve, reject) => {
+			const { isValid, errors } = _ajv.validate("planUpdate", query)
+			if (!isValid) {
+				reject(errors)
+				return
+			}
+
+			paypal.billingPlan.update(planId, query, _config.paypal, (error) => {
+				if (error) {
+					reject(error)
+				} else {
+					resolve(true)
+				}
+			})
+		})
+	}
+
+	return promise.then(sendRequest)
 }
 
 module.exports = planUpdate
