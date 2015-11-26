c066585d0f3e25c85ec02574e72472472ddbb5cf
Dmitry Usa <atskiisotona@gmail.com>
2015-11-27 01:38:36 +0800

    More style fixes

--- src/actions/agreement/create.js
+++ src/actions/agreement/create.js
@@ -5,31 +5,31 @@ const paypal = require('paypal-rest-sdk');
 const url = require('url');
 
 function agreementCreate(agreement) {
-	const {
-		_config
-	} = this;
+  const {
+    _config,
+    } = this;
 
-	let promise = Promise.bind(this);
+  const promise = Promise.bind(this);
 
-	function sendRequest() {
-		return new Promise((resolve, reject) => {
-			paypal.billingAgreement.create(agreement, _config.paypal, (error, newAgreement) => {
-				if (error) {
-					reject(error)
-				} else {
-					const approval = ld.findWhere(newAgreement.links, { rel: 'approval_url' });
-					if (approval === null) {
-						reject(new Errors.NotSupportedError("Unexpected PayPal response!"))
-					} else {
-						const token = url.parse(approval.href, true).query.token;
-						resolve({ token, url: approval.href })
-					}
-				}
-			})
-		})
-	}
+  function sendRequest() {
+    return new Promise((resolve, reject) => {
+      paypal.billingAgreement.create(agreement, _config.paypal, (error, newAgreement) => {
+        if (error) {
+          reject(error);
+        } else {
+          const approval = ld.findWhere(newAgreement.links, {rel: 'approval_url'});
+          if (approval === null) {
+            reject(new Errors.NotSupportedError('Unexpected PayPal response!'));
+          } else {
+            const token = url.parse(approval.href, true).query.token;
+            resolve({token, url: approval.href, agreement: newAgreement});
+          }
+        }
+      });
+    });
+  }
 
-	return promise.then(sendRequest)
+  return promise.then(sendRequest);
 }
 
 module.exports = agreementCreate;
--- src/actions/plan/list.js
+++ src/actions/plan/list.js
@@ -3,29 +3,29 @@ const paypal = require('paypal-rest-sdk');
 
 /**
  * Retrieve list of available Plans
- * @param query https://developer.paypal.com/docs/rest/api/payments.billing-plans/#list
+ * @param message https://developer.paypal.com/docs/rest/api/payments.billing-plans/#list
  * @return plans Array of plans
  */
-function planList(query) {
-	const {
-		_config
-	} = this;
+function planList(message) {
+  const {
+    _config,
+    } = this;
 
-	let promise = Promise.bind(this);
+  const promise = Promise.bind(this);
 
-	function sendRequest() {
-		return new Promise((resolve, reject) => {
-			paypal.billingPlan.list(query, _config.paypal, (error, list) => {
-				if (error) {
-					reject(error)
-				} else {
-					resolve(list)
-				}
-			})
-		})
-	}
+  function sendRequest() {
+    return new Promise((resolve, reject) => {
+      paypal.billingPlan.list(message.query, _config.paypal, (error, list) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(list);
+        }
+      });
+    });
+  }
 
-	return promise.then(sendRequest)
+  return promise.then(sendRequest);
 }
 
 module.exports = planList;
