bdda84a928642619f7a486d43d1efd64e8f3a4e4
Dmitry Usa <atskiisotona@gmail.com>
2015-12-02 23:12:35 +0800

    Fix missing module

--- bin/mservice.js
+++ bin/mservice.js
@@ -11,13 +11,19 @@ if (process.env.NODE_ENV === 'production') {
   dir = '../lib';
 } else {
   dir = '../src';
-  require('../test/babelhook.js');
+  require('babel-core/register')({
+    optional: [ 'es7.objectRestSpread', 'es7.classProperties', 'es7.decorators' ],
+  });
 }
 
 var Service = require(dir);
 var service = new Service(configuration);
-return service.connect()
+service.connect()
+  .then(function serviceUp() {
+    service.log.info('Started service');
+  })
   .catch(function serviceCrashed(err) {
+    service.log.fatal('Failed to start service', err);
     setImmediate(function escapeTryCatch() {
       throw err;
     });
