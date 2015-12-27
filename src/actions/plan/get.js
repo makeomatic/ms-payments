5ea33984677a33d9be7477ee37fe49f351f4fbcd
AVVS <v@aminev.me>
2015-12-27 20:12:40 +0300

    fix: report 404 on missing plan

--- src/actions/plan/get.js
+++ src/actions/plan/get.js
@@ -1,6 +1,6 @@
 const key = require('../../redisKey.js');
 const { hmget } = require('../../listUtils.js');
-
+const Errors = require('common-errors');
 const EXTRACT_FIELDS = ['plan', 'subs', 'alias', 'hidden'];
 const responseParser = hmget(EXTRACT_FIELDS, JSON.parse, JSON);
 
@@ -9,7 +9,14 @@ function planGet(id) {
   const planKey = key('plans-data', id);
 
   return redis
-    .hmget(planKey, EXTRACT_FIELDS)
+    .exists(planKey)
+    .then(exists => {
+      if (!exists) {
+        throw new Errors.HttpStatusError(404, `plan ${id} not found`);
+      }
+
+      return redis.hmget(planKey, EXTRACT_FIELDS);
+    })
     .then(responseParser);
 }
 
