77a45882f282d945e3519a38f481eb5c7cce3bf9
AVVS <v@makeomatic.ru>
2017-05-05 21:16:42 -0700

    feat: transactions aggregate endpoint
    Allows to get some aggregate value per user

--- src/actions/transaction/aggregate.js
+++ src/actions/transaction/aggregate.js
@@ -1,8 +1,7 @@
-/* eslint-disable */
-
 const Promise = require('bluebird');
 const fsort = require('redis-filtered-sort');
 const key = require('../../redisKey');
+const is = require('is');
 
 // helpers
 const { TRANSACTIONS_INDEX, TRANSACTIONS_COMMON_DATA } = require('../../constants.js');
@@ -12,8 +11,14 @@ const { TRANSACTIONS_INDEX, TRANSACTIONS_COMMON_DATA } = require('../../constant
  * @return {Promise}
  */
 module.exports = function listCommonTransactions({ params }) {
-  const { redis } = this;
-  const { owners, type, filter } = params;
+  const { redis, config } = this;
+  const { owners, filter, aggregate } = params;
+
+  // prepare key pattern
+  const pattern = key(TRANSACTIONS_COMMON_DATA, '*');
+  const strFilter = is.string(filter) ? filter : fsort.filter(filter || {});
+  const agg = fsort.filter(aggregate);
+  const prefixLength = config.redis.options.keyPrefix.length;
 
   // PSEUDO-CODE
   // ----------------
@@ -22,8 +27,10 @@ module.exports = function listCommonTransactions({ params }) {
   // 3. we generate ids of indices for the transactions and then run an aggregate on them
 
   return Promise.map(owners, (owner) => {
-    const index = key(TRANSACTIONS_INDEX, owner, type);
-
-
+    const index = key(TRANSACTIONS_INDEX, owner);
+    return redis
+      .fsort(index, pattern, null, 'DESC', strFilter, Date.now(), 0, 0, 5000, 1)
+      .then(idlist => redis.fsortAggregate(idlist.slice(prefixLength), pattern, agg))
+      .then(JSON.parse);
   });
 };
