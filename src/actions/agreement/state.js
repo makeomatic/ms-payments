9d79e3ab72a7eff0424203fe75e29784d51de284
Vitaly Aminev <AVVS@users.noreply.github.com>
2020-05-25 20:10:52 -0700

    fix: correctly detect ignorable error

--- src/actions/agreement/state.js
+++ src/actions/agreement/state.js
@@ -17,6 +17,12 @@ const ACTION_TO_STATE = {
   cancel: 'cancelled',
 };
 
+const isErrorToBeIgnored = (err) => {
+  return  err.httpStatusCode === 400
+      && err.response.name === 'STATUS_INVALID'
+      && err.response.message === 'Invalid profile status for cancel action; profile should be active or suspended';
+};
+
 /**
  * @api {amqp} <prefix>.agreement.state Changes agreement state
  * @apiVersion 1.0.0
@@ -56,11 +62,10 @@ async function agreementState({ params: message }) {
     log.info({ state, agreementId: id, note }, 'updating agreement state');
     await operations[state].call(this, id, { note }, config.paypal);
   } catch (err) {
-    if (err.httpStatusCode !== 400
-      && err.response.name !== 'STATUS_INVALID'
-      && err.response.message !== 'Invalid profile status for cancel action; profile should be active or suspended'
-    ) {
+    if (!isErrorToBeIgnored(err)) {
       throw new Errors.HttpStatusError(err.httpStatusCode, `[${state}] ${id}: ${err.response.message}`, err.response.name);
+    } else {
+      log.warn({ err, state, agreementId: id, note }, 'failed to update agreement state, but can be ignored');
     }
   }
 
