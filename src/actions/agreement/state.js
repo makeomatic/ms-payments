6ccd6ec582fbceaa0d3b50628efce01e680c1db0
Vitaly Aminev <AVVS@users.noreply.github.com>
2020-06-01 10:34:39 -0700

    fix: ignore out of sync cancelled status when we want to cancel (#112)

--- src/actions/agreement/state.js
+++ src/actions/agreement/state.js
@@ -17,6 +17,13 @@ const ACTION_TO_STATE = {
   cancel: 'cancelled',
 };
 
+const isErrorToBeIgnored = (err) => {
+  return err.httpStatusCode === 400
+      && err.response
+      && err.response.name === 'STATUS_INVALID'
+      && err.response.message === 'Invalid profile status for cancel action; profile should be active or suspended';
+};
+
 /**
  * @api {amqp} <prefix>.agreement.state Changes agreement state
  * @apiVersion 1.0.0
@@ -56,7 +63,12 @@ async function agreementState({ params: message }) {
     log.info({ state, agreementId: id, note }, 'updating agreement state');
     await operations[state].call(this, id, { note }, config.paypal);
   } catch (err) {
-    throw new Errors.HttpStatusError(err.httpStatusCode, `[${state}] ${id}: ${err.response.message}`, err.response.name);
+    if (!isErrorToBeIgnored(err)) {
+      log.error({ err, state, agreementId: id, note }, 'failed to update agreement state');
+      throw new Errors.HttpStatusError(err.httpStatusCode, `[${state}] ${id}: ${err.response.message}`, err.response.name);
+    } else {
+      log.warn({ err, state, agreementId: id, note }, 'failed to update agreement state, but can be ignored');
+    }
   }
 
   await this.dispatch('transaction.sync', {
