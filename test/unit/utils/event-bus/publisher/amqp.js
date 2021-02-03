039b2c2077b59a2e3d8ade9eef255f68e492852a
Olga Ivanova <belkinadasha@gmail.com>
2021-02-03 22:25:18 +0500

    test: improve test coverage

--- test/unit/utils/event-bus/publisher/amqp.js
+++ test/unit/utils/event-bus/publisher/amqp.js
@@ -1,7 +1,28 @@
-const assert = require('assert');
+const sinon = require('sinon');
+const { AMQPPublisher } = require('../../../../../src/utils/event-bus/publisher');
 
 describe('AMQP Publisher', () => {
-  it('should be able to publish', () => {
-    assert.ok('true');
+  const sandbox = sinon.createSandbox();
+
+  afterEach(() => sandbox.reset());
+
+  const logFake = sandbox.fake();
+  const amqpStub = sinon.stub({ publishAndWait: async () => {} });
+
+  it('should be able to publish', async () => {
+    amqpStub.publishAndWait.resolves();
+    const publisher = new AMQPPublisher(amqpStub, logFake);
+    await publisher.publish('route', { some: 'message' });
+
+    sandbox.assert.calledOnceWithExactly(
+      amqpStub.publishAndWait,
+      'route',
+      sinon.match({ some: 'message' }),
+      sinon.match({
+        confirm: true,
+        mandatory: true,
+        deliveryMode: 2,
+      })
+    );
   });
 });
