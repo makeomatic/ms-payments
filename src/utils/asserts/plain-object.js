0ea3717f846449b8bf3cb27fd27cea4433cfb085
Sasha Sorokin <sorokin.sh@gmail.com>
2021-06-24 14:20:20 +0400

    chore: removed unused code (stripe, balance, charge)

--- src/utils/asserts/plain-object.js
+++
@@ -1,6 +0,0 @@
-const { strictEqual } = require('assert');
-const isPlainObject = require('lodash/isPlainObject');
-
-module.exports = function assertPlainObject(value, error) {
-  strictEqual(isPlainObject(value), true, error);
-};
--- src/utils/asserts/string.js
+++
@@ -1,6 +0,0 @@
-const { strictEqual } = require('assert');
-const isString = require('lodash/isString');
-
-module.exports = function assertString(value, error) {
-  strictEqual(isString(value), true, error);
-};
