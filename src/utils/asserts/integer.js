cc8f5ec55222a652322135c11e46bd5d203eeeee
Sasha S <sorokin.sh@gmail.com>
2021-11-05 22:44:53 +0400

    chore: remove unused code (#157)
    * chore: remove unused code - stripe
    * chore: remove unused code - charge, balance, paypal-payments

--- src/utils/asserts/integer.js
+++
@@ -1,6 +0,0 @@
-const { strictEqual } = require('assert');
-const isInteger = require('lodash/isInteger');
-
-module.exports = function assertInteger(value, error) {
-  strictEqual(isInteger(value), true, error);
-};
--- src/utils/asserts/string-not-empty.js
+++
@@ -1,7 +0,0 @@
-const { strictEqual } = require('assert');
-const isString = require('lodash/isString');
-
-module.exports = function assertStringNotEmpty(value, error) {
-  strictEqual(isString(value), true, error);
-  strictEqual(value.length !== 0, true, error);
-};
